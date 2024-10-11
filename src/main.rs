be32164854f9d58a5ec3249bfc04180fabb66d96
mrtewogbade <johndharey@gmail.com>
2024-10-11 13:21:15 +0100

    azure

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,9 @@ use aws_sdk_dynamodb::model::AttributeValue;
 use lambda_runtime::{handler_fn, Context, Error as LambdaError};
 use serde::Deserialize;
 use serde_json::{json, Value};
+use uuid::Uuid;
 
-fn main (){}
+#[tokio::main]
+async fn main () -> Results<(), LambdaError> {
+    
+}
