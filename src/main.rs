cf818d4019fddf3640de0ab7cca2f4f9494adf3a
mrtewogbade <johndharey@gmail.com>
2024-10-11 13:13:41 +0100

    aws

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_dynamodb::Client;
+use aws_sdk_dynamodb::model::AttributeValue;
+use lambda_runtime::{handler_fn, Context, Error as LambdaError};
+use serde::Deserialize;
+use serde_json::{json, Value};
+
+fn main (){}
